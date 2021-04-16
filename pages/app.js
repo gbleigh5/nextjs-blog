1dc885e1cb2c6cdfe835c4e6bea01531e9b35dfb
gbleigh5 <gbleigh5@gmail.com>
2021-04-16 15:26:44 +0300

    My first commit to nextjs-blog

---
+++ pages/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/global.css'
+
+export default function App({Component, pageProps}) {
+    return (
+        <div>
+            {Component, pageProps}
+        </div>
+    )
+}
